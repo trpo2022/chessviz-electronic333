1c58c71ac30a33a9d6f378b95b84b0e67031e021
Саша Боков <alex.skaut2@yandex.ru>
2022-05-14 15:30:29 +0700

    Функции доступа к полям доски для шахмат

--- src/chess-logic/chess-logic.h
+++ src/chess-logic/chess-logic.h
@@ -3,8 +3,8 @@
 // Чтобы не переусложнять, моя доска для шахмат 
 // будет константного размера 8х8.
 // Никаких извращений типа 8х10.
-#define WIDTH 8 // Ширина шахматной доски
-#define HEIGTH 8 // Её высота
+#define BOARD_WIDTH 8 // Ширина шахматной доски
+#define BOARD_HEIGTH 8 // Её высота
 
 // По FEN нотации
 #define BLACK_PAWN 'p' // Пешки
@@ -26,4 +26,9 @@
 // 'P' - белая пешка,
 // 'b' - чёрный слон,
 // ...
-typedef char* chess_board
+typedef char* chess_board;
+typedef char piece;
+
+piece get_at (chess_board, int, int); // Возвращает фигуру по координатам
+void set_at (chess_board, int, int, piece); // Устанавливает фигуру по координатам
+// Где int, int in [0 to BOARD_HEIGTH], [0 to BOARD_WIDTH]
